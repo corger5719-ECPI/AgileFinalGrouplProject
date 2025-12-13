ed0aa3056102fcd67c05aedd201b5cc96bbe4601
corger5719-ECPI <cora.germany@gmail.com>
2025-12-13 15:40:32 -0500

    Create optionSelector.h

---
+++ optionSelector.h
@@ -0,0 +1,11 @@
+#ifndef OPTION_SELECTOR_H
+#define OPTION_SELECTOR_H
+
+/*
+ * Function: mainLoop
+ * Purpose: Executes program functionality based on menu selection
+ * Parameter: option - integer selected by user
+ */
+void mainLoop(int option);
+
+#endif
