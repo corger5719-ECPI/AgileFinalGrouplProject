699ad66180b53669bd7326aacba38907b378f383
corger5719-ECPI <cora.germany@gmail.com>
2025-12-13 15:42:15 -0500

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+/*
+ * Function: menu
+ * Purpose: Displays the menu options to the user
+ * Returns: Integer representing the user's selected option
+ */
+int menu(void);
+
+#endif
