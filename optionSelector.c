0612f241bcb6f4c9d5eed93aef2e7a7016c4dc0e
corger5719-ECPI <cora.germany@gmail.com>
2025-12-13 19:39:11 -0500

    Update optionSelector.c

--- optionSelector.c
+++ optionSelector.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "optionSelector.h"
-#include "writeFile.h"
-#include "readFile.h"
 
-/*
- * Function: mainLoop
- * Purpose: Calls appropriate functions based on the user's menu choice
- */
 void mainLoop(int option)
 {
     switch (option)
     {
         case 1:
-            writeFile();
+            printf("Option One selected.\n");
             break;
 
         case 2:
-            readFile();
+            printf("Option Two selected.\n");
             break;
 
         case 3:
-            printf("Calculation 1 executed.\n");
+            printf("Writing to file...\n");
             break;
 
         case 4:
-            printf("Calculation 2 executed.\n");
+            printf("Exiting program...\n");
             break;
 
         default:
-            printf("Invalid option selected.\n");
+            printf("Invalid selection.\n");
     }
 
-    // Pause so the user can read output before returning to menu
-    printf("\nPress Enter to continue...");
-    getchar(); // clear buffer
-    getchar(); // wait for Enter
+    system("pause");
 }
